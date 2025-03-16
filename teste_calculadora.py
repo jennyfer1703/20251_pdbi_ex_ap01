190fdc8a855c56ef7e3670c3f97844e73f04920d
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-03-16 19:46:41 -0300

    test: testa a divisao

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -3,4 +3,5 @@ a = 2
 b = 2
 
 print(f'{a} + {b} = {calculadora.somar(a, b)}')
-print(f'{a} * {b} = {calculadora.somar(a, b)}')
+print(f'{a} * {b} = {calculadora.multiplicar(a, b)}')
+print(f'{a} / {b} = {calculadora.dividir(a, b)}')
